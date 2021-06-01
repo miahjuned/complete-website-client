357f65e9fc250e50e29b78c40fd004aaa476e41b
zunead <zuned1996@outlook.com>
2021-06-02 00:08:38 +0600

    updated

--- src/component/Featured/Featured.js
+++ src/component/Featured/Featured.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 import './Featured.css';
 import Ipt from '../../images/IPT.jpg';
 import onlineTrainer from '../../images/Certified-Online-Trainer.jpg';
@@ -11,50 +11,23 @@ const Featured = () => {
         <Container>
             <h2 className="featuredTitle">Featured on:</h2>
             <hr/>
-            <Row className="featuredBody" >
-                <Col  md={3} lg={3} sm={6} xl={3} xs={6}>
-                    <a href="#" target="_blank" >
-                        {/* <FigureImage
-                            width={80}
-                            alt="IPT"
-                            src={Ipt}
-                        /> */}
-
-                        <img className="Ipt" src={Ipt} alt=""/>
-                    </a>
-                </Col>
-                <Col md={3} lg={3} sm={6} xl={3} xs={6}>
-                    <a href="#" target="_blank">
-                        {/* <FigureImage
-                            width={200}
-                            alt="Certified Online Trainer"
-                            src={onlineTrainer}
-                        /> */}
-
-                        <img className="align-middle onlineTrainer" src={onlineTrainer} alt=""/>
-                    </a>
-                </Col>
-                <Col md={3} lg={3} sm={6} xl={3} xs={6}>
-                    <a href="#home">
-                        {/* <FigureImage
-                            width={150}
-                            alt="BBC"
-                            src={BBC}
-                        /> */}
-
-                        <img className="BBC" src={BBC} alt=""/>
-                    </a>
-                </Col>
-                <Col  md={3} lg={3} sm={6} xl={3} xs={6}>
-                    <a href="#" target="_blank">
-                        {/* <FigureImage
-                            width={100}
-                            href="#"
-                            alt="Brithsh Mushlim Radio"
-                            src={BrithshMushlim}
-                        /> */}
-                        <img className="BritishMuslim" src={BrithshMushlim} alt=""/>
-                    </a>
+            <Row >
+                <Col  md={12} lg={12} sm={12} xl={12} xs={12}>
+                    <div className="featured d-flex flex-wrap">
+                        <a href="#" target="_blank" >
+                            <Image src={Ipt} fluid  />
+                        </a>
+                        
+                        <a href="#" target="_blank" rel="noreferrer">
+                            <Image src={BrithshMushlim} fluid  />
+                        </a>
+                        <a href="#" target="_blank">
+                            <Image src={onlineTrainer} fluid  />
+                        </a>  
+                        <a href="#home">
+                            <Image src={BBC} fluid  />
+                        </a>
+                    </div>
                 </Col>
             </Row>
         </Container>
